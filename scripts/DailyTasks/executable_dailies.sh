53fb2b2890525a1f0f3d9a5463c6b7ac1ee0b482
TheWolf2068 <mbavli51@gmail.com>
2024-12-27 09:54:47 +0100

    Removed stupid stuff that would only clutter the dotfiles

--- scripts/DailyTasks/executable_dailies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pacman -Qqen > /home/malenia/.local/share/chezmoi/scripts/DailyTasks/pkglist-repo.txt
-pacman -Qqem > /home/malenia/.local/share/chezmoi/scripts/DailyTasks/pkglist-aur.txt
-
-cd /home/malenia/.local/share/chezmoi
-git add "$HOME/.local/share/chezmoi/scripts/DailyTasks/*"
-git commit -m "Updated package list"
-git  push -u origin main
