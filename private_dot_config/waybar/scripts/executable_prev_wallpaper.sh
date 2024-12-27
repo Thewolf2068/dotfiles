cfc3a265955b6893a5c70905f63e72267535b6be
TheWolf2068 <mbavli51@gmail.com>
2024-12-27 01:43:03 +0100

    Added Waybar

---
+++ private_dot_config/waybar/scripts/executable_prev_wallpaper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export SWWW_TRANSITION_POS=20,1060
+export SWWW_TRANSITION=grow
+
+
+# Define the wallpapers directory
+WALLPAPER_DIR="$HOME/.config/waybar/scripts/Wallpapers"
+
+# Get the current wallpaper file (assuming the current wallpaper is the one set by swww)
+CURRENT_WALLPAPER=$(swww query)
+
+# Get all the wallpapers in the directory, sorted alphabetically
+WALLPAPERS=($(ls "$WALLPAPER_DIR" | sort))
+
+# Find the index of the current wallpaper in the sorted list
+CURRENT_INDEX=-1
+for i in "${!WALLPAPERS[@]}"; do
+    if [[ "${WALLPAPERS[$i]}" == "$(basename "$CURRENT_WALLPAPER")" ]]; then
+        CURRENT_INDEX=$i
+        break
+    fi
+done
+
+# Determine the previous wallpaper, cycling back to the end if needed
+PREVIOUS_INDEX=$(( (CURRENT_INDEX - 1 + ${#WALLPAPERS[@]}) % ${#WALLPAPERS[@]} ))
+
+# Set the previous wallpaper
+PREVIOUS_WALLPAPER="$WALLPAPER_DIR/${WALLPAPERS[$PREVIOUS_INDEX]}"
+swww img "$PREVIOUS_WALLPAPER"
