e720ef58afdf17718f6d7e6e2220bdc8e596129d
TheWolf2068 <mbavli51@gmail.com>
2024-12-27 09:41:44 +0100

    Updated package script

--- scripts/DailyTasks/executable_dailies.sh
+++ scripts/DailyTasks/executable_dailies.sh
@@ -2,3 +2,8 @@
 
 pacman -Qqen > ./pkglist-repo.txt
 pacman -Qqem > ./pkglist-aur.txt
+
+chezmoi cd
+git add "$HOME/.local/share/chezmoi/scripts/DailyTasks/*"
+git commit -m "Updated package list"
+git  push -u origin main
