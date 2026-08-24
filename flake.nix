6734b9e46734c00298d3bf0d7e33edd0aa8e4adc
TheWolf2068 <29edt9l2@4wrd.cc>
2026-08-24 20:31:11 +0200

    fixed error in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,19 +4,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Catppuccin
     catppuccin = {
       url = "github:catppuccin/nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Zen browser
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs = {
@@ -25,126 +22,84 @@
       };
     };
 
-    # Firefox/zen browser extensions
     nur = {
       url = "github:nix-community/nur";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-
-    # Kotofetch (non-flake source)
     kotofetch-src = {
       url = "github:hxpe-dev/kotofetch";
       flake = false;
     };
 
-    # Waybar (bleeding edge, kept separate for mango workspaces + cava support)
     waybar = {
       url = "github:Alexays/Waybar";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Spicetify
-    spicetify-nix = {
-      url = "github:Gerg-L/spicetify-nix";
-    };
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
 
-    # Neovim
     nvf = {
-      url = "github:notashelf/nvf"; 
+      url = "github:notashelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Discord configuration
     nixcord.url = "github:4evy/nixcord";
   };
 
-  outputs = { self, nixpkgs, home-manager, catppuccin, zen-browser, kotofetch-src, waybar, spicetify-nix, nvf, nur, ... }@inputs: {
-    nixosConfigurations.thinkpadofmiquella = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        # Overlays: kotofetch + waybar (built with mango workspaces + cava support)
-        ({ config, pkgs, ... }: {
-          nixpkgs.overlays = [
-            inputs.nur.overlays.default
-            (final: prev: {
-              kotofetch = final.callPackage kotofetch-src { };
-            })
-            (final: prev: {
-              waybar = (prev.callPackage "${waybar}/nix/default.nix" {
-                waybar = prev.waybar;
-                version = waybar.shortRev or "dirty";
-              }).overrideAttrs (old: {
-                mesonFlags = (old.mesonFlags or []) ++ [
-                  "-Dcava=enabled"
-                  "-Dmango=true"
+  outputs = { self, nixpkgs, home-manager, catppuccin, zen-browser, kotofetch-src, waybar, spicetify-nix, nvf, nur, ... }@inputs:
+    let
+      # Shared overlay: kotofetch + waybar (built with mango workspaces + cava support)
+      overlaysModule = { config, pkgs, ... }: {
+        nixpkgs.overlays = [
+          inputs.nur.overlays.default
+          (final: prev: {
+            kotofetch = final.callPackage kotofetch-src { };
+          })
+          (final: prev: {
+            waybar = (prev.callPackage "${waybar}/nix/default.nix" {
+              waybar = prev.waybar;
+              version = waybar.shortRev or "dirty";
+            }).overrideAttrs (old: {
+              mesonFlags = (old.mesonFlags or []) ++ [
+                "-Dcava=enabled"
+                "-Dmango=true"
+              ];
+            });
+          })
+        ];
+      };
+
+      # Builds one host's nixosSystem given its hostname (used for both the
+      # configuration.nix and home.nix paths under ./hosts/<hostname>/)
+      mkHost = hostname:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            overlaysModule
+            ./hosts/${hostname}/configuration.nix
+            catppuccin.nixosModules.catppuccin
+            home-manager.nixosModules.home-manager
+            {
+              nixpkgs.config.allowUnfree = true;
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.users.malenia = {
+                imports = [
+                  ./hosts/${hostname}/home.nix
+                  catppuccin.homeModules.catppuccin
+                  inputs.spicetify-nix.homeManagerModules.default
+                  inputs.nvf.homeManagerModules.default
                 ];
-              });
-            })
+              };
+            }
           ];
-        })
-        ./hosts/thinkpadofmiquella/configuration.nix
-        catppuccin.nixosModules.catppuccin
-        home-manager.nixosModules.home-manager
-        {
-          nixpkgs.config.allowUnfree = true;
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.users.malenia = {
-            imports = [
-              ./hosts/thinkpadofmiquella/home.nix
-              catppuccin.homeModules.catppuccin
-              inputs.spicetify-nix.homeManagerModules.default
-              inputs.nvf.homeManagerModules.default
-            ];
-          };
-        }
-      ];
+        };
+    in
+    {
+      nixosConfigurations.thinkpadofmiquella = mkHost "thinkpadofmiquella";
+      nixosConfigurations.desktopofmiquella = mkHost "desktopofmiquella";
     };
-    nixosConfigurations.desktopofmiquella= nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        # Overlays: kotofetch + waybar (built with mango workspaces + cava support)
-        ({ config, pkgs, ... }: {
-          nixpkgs.overlays = [
-            inputs.nur.overlays.default
-            (final: prev: {
-              kotofetch = final.callPackage kotofetch-src { };
-            })
-            (final: prev: {
-              waybar = (prev.callPackage "${waybar}/nix/default.nix" {
-                waybar = prev.waybar;
-                version = waybar.shortRev or "dirty";
-              }).overrideAttrs (old: {
-                mesonFlags = (old.mesonFlags or []) ++ [
-                  "-Dcava=enabled"
-                  "-Dmango=true"
-                ];
-              });
-            })
-          ];
-        })
-        ./hosts/thinkpadofmiquella/configuration.nix
-        catppuccin.nixosModules.catppuccin
-        home-manager.nixosModules.home-manager
-        {
-          nixpkgs.config.allowUnfree = true;
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.users.malenia = {
-            imports = [
-              ./hosts/thinkpadofmiquella/home.nix
-              catppuccin.homeModules.catppuccin
-              inputs.spicetify-nix.homeManagerModules.default
-              inputs.nvf.homeManagerModules.default
-            ];
-          };
-        }
-      ];
-    };
-  };
 }
