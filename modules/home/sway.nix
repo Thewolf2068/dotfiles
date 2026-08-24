6d353ade834a62a77f94d4aabba7be1698b2b7af
TheWolf2068 <29edt9l2@4wrd.cc>
2026-08-24 18:34:29 +0200

    fixed small

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 let
-  wmScript = "${./scripts/workspace_manager.sh}";
+wmScript = "${./scripts/workspace_manager.sh}";
 in
 {
   wayland.windowManager.sway = {
@@ -23,18 +23,11 @@ in
       bars = [ ];
 
       startup = [
-<<<<<<< HEAD
-      { command = "zen-beta"; }
-      { command = "swaymsg create_output HEADLESS-1"; }
-      { command = "swayosd-server"; }
-      { command = "swaync"; }
-=======
         { command = "zen-beta"; }
         { command = "swaymsg create_output HEADLESS-1"; }
         { command = "swayosd-server"; }
         { command = "swaync"; }
         { command = "sh -c 'protonplus update all; exec steam -silent'"; }
->>>>>>> 35eb84460c352d1d5f3e92256ac3db063aeb2c7a
       ];
 
       gaps.inner = 5;
