63e665febb7f2b5f3d0faed4139a705ae3d95335
TheWolf2068 <29edt9l2@4wrd.cc>
2026-08-25 00:04:32 +0200

    sunshine server specific adjustments

--- modules/home/scripts/headless_toggle.sh
+++ modules/home/scripts/headless_toggle.sh
@@ -1,36 +1,19 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-mapfile -t monitors < <(
-    /usr/bin/env mmsg get all-monitors |
-        /usr/bin/env jq -r '.monitors[].name'
-)
+HEADLESS="HEADLESS-1"
+PHYSICAL_ACTIVE=$(swaymsg -t get_outputs | jq -r ".[] | select(.name != \"$HEADLESS\" and .active == true) | .name")
 
-has_headless=false
-
-for monitor in "${monitors[@]}"; do
-    if [[ "$monitor" == *HEADLESS* ]]; then
-        has_headless=true
-        break
-    fi
-done
-
-if $has_headless; then
-    # Physical monitors mode
-    /usr/bin/env mmsg dispatch destroy_all_virtual_output
-
-    for monitor in "${monitors[@]}"; do
-        if [[ "$monitor" != *HEADLESS* ]]; then
-            /usr/bin/env mmsg dispatch "enable_monitor,name:${monitor}"
-        fi
+if [ -n "$PHYSICAL_ACTIVE" ]; then
+    swaymsg output "HEADLESS-1" enable
+    for mon in $PHYSICAL_ACTIVE; do
+        swaymsg output "$mon" disable
     done
+    swaymsg workspace 99
 else
-    # Headless/virtual output mode
-    /usr/bin/env mmsg dispatch create_virtual_output
-
-    for monitor in "${monitors[@]}"; do
-        if [[ "$monitor" != *HEADLESS* ]]; then
-            /usr/bin/env mmsg dispatch "disable_monitor,name:${monitor}"
-        fi
-    done
+    swaymsg output "*" enable
+    swaymsg output "HEADLESS-1" disable
+    # pkill waybar
+    # waybar & 
+    swaymsg workspace "99"
+    # swaymsg move workspace to output '"LG Electronics LG ULTRAGEAR 407NTVS68263"'
 fi
--- modules/system/gaming.nix
+++ modules/system/gaming.nix
@@ -16,6 +16,9 @@
   services.sunshine = {
     enable = true;
     openFirewall = true;
+    settings = {
+      back_button_timeout = 2000;
+    };
     applications = {
       apps = [
         {
