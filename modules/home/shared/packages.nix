cb6505a3fea46e8cd3a63eea772f16b7d1ec9064
TheWolf2068 <29edt9l2@4wrd.cc>
2026-08-26 01:29:00 +0200

    cleaned up packages

--- modules/home/shared/packages.nix
+++ modules/home/shared/packages.nix
@@ -1,54 +1,41 @@
-{ pkgs, inputs, system, ... }:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
-    neovim              # unless you want nvim.nix to own this via programs.neovim
-    foot
     kitty
-    rofi
     fuzzel
     tree
     wl-clipboard
-    # inputs.zen-browser.packages."${pkgs.system}".default
     swaybg
-    # discord
     signal-desktop
-    zed-editor
-    swaynotificationcenter
-    nil
-    nixd
-    libnotify
-    swayosd
     kotofetch
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    river
     lswt
-    swaylock-effects
     jq
     eza
     playerctl
-    (pkgs.python3.withPackages (ps: with ps; [
-      pygobject3
-    ]))
-    gobject-introspection
     wlr-randr
     fzf
     bat
     btop
     cava
     localsend
-    obsidian
     opencode
     networkmanagerapplet
     pavucontrol
     wev
     tealdeer
+
+
+    # Gaming
     supertuxkart
-    clipse
-    wayfreeze
-    grim
-    slurp
-    swappy
+    mangohud
+    protonplus
+    lutris
+    heroic
+    prismlauncher
+    limo
+    deadlock-mod-manager
+    faugus-launcher
+    sgdboop
   ];
 
   home.sessionVariables = {
--- modules/system/gaming.nix
+++ modules/system/gaming.nix
@@ -5,9 +5,7 @@
     gamescopeSession.enable = true;
   };
   programs.gamemode.enable = true;
-  programs.gamescope = {
-    enable = true;
-  };
+  programs.gamescope.enable = true;
 
   # AMD GPU control - fan curves, power limits, clock offsets
   services.lact.enable = true;
@@ -39,24 +37,5 @@
       ];
     };
   };
-
-
-
-        
-
-  # Controller support beyond the default xpad-covered devices
   services.udev.packages = [ pkgs.game-devices-udev-rules ];
-
-  environment.systemPackages = with pkgs; [
-    mangohud
-    protonplus
-    lutris
-    heroic
-    prismlauncher
-    limo
-    deadlock-mod-manager
-    faugus-launcher
-    sgdboop
-    protonplus
-  ];
 }
