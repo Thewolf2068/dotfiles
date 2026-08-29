0c4a770ffddcf8de77828811209651a709462f47
TheWolf2068 <29edt9l2@4wrd.cc>
2026-08-29 04:09:46 +0200

    enabled flatpak

--- modules/system/common.nix
+++ modules/system/common.nix
@@ -4,6 +4,7 @@
       optimise.automatic = true;
       settings.experimental-features = [ "nix-command" "flakes" ];
     };
+    services.flatpak.enable = true;
     services.power-profiles-daemon.enable = true;
     programs.kdeconnect.enable = true;
 
